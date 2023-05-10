4c9aa79015e299ff203e7c37bcfd441b33295a5c
yo <126212399+JakeWad@users.noreply.github.com>
2023-05-10 11:45:58 -0500

    Update extracredit.py

--- extracredit.py
+++ extracredit.py
@@ -1,12 +1,1 @@
-import sys
-
-# Get filename and text from command line arguments
-filename = sys.argv[1]
-text = sys.argv[2]
-
-# Remove quotation marks from text
-text = text.strip('"')
-
-# Write text to file
-with open(filename, 'w') as f:
-    f.write(text)
+extracredit.py
